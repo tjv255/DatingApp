aa0e1e5fdb78b2e0eba57d1a02439f5113e1f599
tjv255 <tjv255@mail.usask.ca>
2022-08-30 15:56:30 -0600

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,16 @@ export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: res => this.users = res,
-      error: error => console.log(error)
-    });
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user')); 
+    this.accountService.setCurrentUser(user);
+  }
+
+
 }
