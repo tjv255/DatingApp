d3f0d302c4204f0e24a90c48988dc685c833a7fa
tjv255 <tjv255@mail.usask.ca>
2022-09-21 16:04:13 -0600

    End of section 17

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,27 +2,29 @@ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
-  public static class ApplicationServiceExtensions
-  {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    public static class ApplicationServiceExtensions
     {
-      services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-      services.AddScoped<ITokenService, TokenService>();
-      services.AddScoped<IPhotoService, PhotoService>();
-      services.AddScoped<ILikesRepository, LikesRepository>();
-      services.AddScoped<IMessageRepository, MessageRepository>();
-      services.AddScoped<LogUserActivity>();
-      services.AddScoped<IUserRepository, UserRepository>();
-      services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlite(config.GetConnectionString("DefaultConnection"));
-      });
-      return services;
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSingleton<PresenceTracker>();
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<LogUserActivity>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
     }
-  }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,26 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Dating app';
-  users: any;
+    title = 'The Dating app';
+    users: any;
 
-  constructor(private accountService: AccountService) {}
+    constructor(private accountService: AccountService, private presence: PresenceService) { }
 
-  ngOnInit() {
-    this.setCurrentUser();
-  }
+    ngOnInit() {
+        this.setCurrentUser();
+    }
 
-  setCurrentUser() {
-    const user: User = JSON.parse(localStorage.getItem('user')); 
-    this.accountService.setCurrentUser(user);
-  }
-
-
+    setCurrentUser() {
+        const user: User = JSON.parse(localStorage.getItem('user'));
+        if (user) {
+            this.accountService.setCurrentUser(user);
+            this.presence.createHubConnection(user);
+        }
+    }
 }
