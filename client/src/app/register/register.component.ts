c1e8583fc19a331df4775e4e35dfd4499093220c
Gurjas604 <33743596+Gurjas604@users.noreply.github.com>
2022-09-29 11:52:03 -0700

    Added new features to register componenet

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -34,7 +34,11 @@ export class RegisterComponent implements OnInit {
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8), this.strong()]],
+      occupation: [''],
+      skills: [''],
+      genres: [''],
+      affiliation: [[]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     });
     this.registerForm.controls.password.valueChanges.subscribe(() => {
