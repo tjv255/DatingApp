f4e45aecb7c0c08af90eaeba179be3ad2b3fcce4
nathansom <nathan.sa.official@gmail.com>
2022-10-04 09:32:41 -0700

    updated OrgLike GET response

--- API/DTOs/OrgLikeDto.cs
+++ API/DTOs/OrgLikeDto.cs
@@ -3,15 +3,19 @@ namespace API.DTOs
     public class OrgLikeDto
     {
         public int OrgId { get; set; }
-        public string Name { get; set; }
+        public string Organization { get; set; }
         public string Introduction { get; set; }
-        public string PhotoUrl { get; set; }
-        public int Established { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime LastUpdated { get; set; } = DateTime.Now;
-        public string City { get; set; }
-        public string ProvinceOrState { get; set; }
-        public string Country { get; set; }
-
+        public string OrganizationCity { get; set; }
+        public string OrganizationState { get; set; }
+        public string OrganizationCountry { get; set; }
+        public string OrganizationPhotoUrl { get; set; }
+        public int LikedUserId { get; set; }
+        public string LikedUserFirstname { get; set; }
+        public string LikedUserLastname { get; set; }
+        public string LikedUserKnownAs { get; set; }
+        public string LikedUserCity { get; set; }
+        public string LikedUserState { get; set; }
+        public string LikedUserCountry { get; set; }
+        public string LikedUserPhotoUrl { get; set; }
     }
 }
