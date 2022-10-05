8219390573f78d8b9e06b0b12aeb6b75d709d893
nathansom <nathan.sa.official@gmail.com>
2022-10-04 17:29:57 -0700

    added org, job, and user filters to OrgRepo

--- API/Data/OrganizationRepository.cs
+++ API/Data/OrganizationRepository.cs
@@ -33,8 +33,18 @@ namespace API.Data
             var organizations = _context.Organizations.AsQueryable();
             var query = organizations;
 
+            if (organizationParams.Name != null)
+                query = query.Where(o => o.Name.ToLower().Trim().Contains(organizationParams.Name.ToLower().Trim())).AsQueryable();
+            if (organizationParams.OrgType != null)
+                query = query.Where(o => o.OrgType.ToLower().Trim().Contains(organizationParams.OrgType.ToLower().Trim())).AsQueryable();
             if (organizationParams.Established != null && organizationParams.Established > 0)
                 query = query.Where(o => o.Established == organizationParams.Established).AsQueryable();
+            if (organizationParams.City != null)
+                query = query.Where(o => o.City.ToLower().Trim().Contains(organizationParams.City.ToLower().Trim())).AsQueryable();
+            if (organizationParams.ProvinceOrState != null)
+                query = query.Where(o => o.ProvinceOrState.ToLower().Trim().Contains(organizationParams.ProvinceOrState.ToLower().Trim())).AsQueryable();
+            if (organizationParams.Country != null)
+                query = query.Where(o => o.Country.ToLower().Trim().Contains(organizationParams.Country.ToLower().Trim())).AsQueryable();
 
             query = organizationParams.OrderBy switch
             {
@@ -111,6 +121,19 @@ namespace API.Data
             var query = _context.Users.Where(u => u.Affiliation.Contains(org));
             query = query.Where(u => u.UserName != userParams.CurrentUsername);
 
+            if (userParams.Occupation != null)
+                query = query.Where(u => u.Occupation.ToLower().Trim().Contains(userParams.Occupation.ToLower().Trim()));
+            if (userParams.Skill != null)
+                query = query.Where(u => u.Skills.ToLower().Trim().Contains(userParams.Skill.ToLower().Trim()));
+            if (userParams.Genre != null)
+                query = query.Where(u => u.Genres.ToLower().Trim().Contains(userParams.Genre.ToLower().Trim()));
+            if (userParams.City != null)
+                query = query.Where(u => u.City.ToLower().Trim().Contains(userParams.City.ToLower().Trim()));
+            if (userParams.ProvinceOrState != null)
+                query = query.Where(u => u.ProvinceOrState.ToLower().Trim().Contains(userParams.ProvinceOrState.ToLower().Trim()));
+            if (userParams.Country != null)
+                query = query.Where(u => u.Country.ToLower().Trim().Contains(userParams.Country.ToLower().Trim()));
+
             query = userParams.OrderBy switch
             {
                 "created" => query.OrderByDescending(u => u.Created),
@@ -128,15 +151,30 @@ namespace API.Data
         {
             var org = _context.Organizations.SingleOrDefault(o => o.Id == id);
             var query = _context.Jobs.Where(j => j.Organization.Equals(org));
-            var emptyOrg = _context.Organizations.Take(0);
 
-            if (query == null)
-                return new PagedList<JobDto>(new List<JobDto>(), 0, jobParams.PageNumber, jobParams.PageSize);
-            
+            if (jobParams.Title != null)
+                query = query.Where(j => j.Title.ToLower().Trim().Contains(jobParams.Title.ToLower().Trim())).AsQueryable();
+            if (jobParams.JobType != null)
+                query = query.Where(j => j.JobType.ToLower().Trim().Contains(jobParams.JobType.ToLower().Trim())).AsQueryable();
+            if (jobParams.PosterID != null && jobParams.PosterID > 0)
+                query = query.Where(j => j.JobPoster.Id == jobParams.PosterID).AsQueryable();
+            if (jobParams.Genres != null)
+                query = query.Where(j => j.Genres.ToLower().Trim().Contains(jobParams.Genres.ToLower().Trim())).AsQueryable().AsQueryable();
+            if (jobParams.SkillsRequired != null)
+                query = query.Where(j => j.SkillsRequired.ToLower().Trim().Contains(jobParams.SkillsRequired.ToLower().Trim())).AsQueryable().AsQueryable();
+            if (jobParams.City != null)
+                query = query.Where(j => j.City.ToLower().Trim().Contains(jobParams.City.ToLower().Trim())).AsQueryable().AsQueryable();
+            if (jobParams.ProvinceOrState != null)
+                query = query.Where(j => j.ProvinceOrState.ToLower().Trim().Contains(jobParams.ProvinceOrState.ToLower().Trim())).AsQueryable().AsQueryable();
+            if (jobParams.Country != null)
+                query = query.Where(j => j.Country.ToLower().Trim().Contains(jobParams.Country.ToLower().Trim())).AsQueryable().AsQueryable();
+
             query = jobParams.OrderBy switch
             {
-                "deadline" => query.OrderByDescending(u => u.Deadline),
-                _ => query.OrderByDescending(u => u.LastUpdated)
+                "alphabetical" => query.OrderBy(o => o.Title),
+                "deadline" => query.OrderByDescending(o => o.Deadline),
+                "lastUpdated" => query.OrderByDescending(o => o.LastUpdated),
+                _ => query.OrderByDescending(o => o.DateCreated)
             };
 
             query = query
@@ -156,6 +194,19 @@ namespace API.Data
             var organizations = _context.Organizations.Where(o => o.OwnerId == id);
             var query = organizations;
 
+            if (organizationParams.Name != null)
+                query = query.Where(o => o.Name.ToLower().Trim().Contains(organizationParams.Name.ToLower().Trim())).AsQueryable();
+            if (organizationParams.OrgType != null)
+                query = query.Where(o => o.OrgType.ToLower().Trim().Contains(organizationParams.OrgType.ToLower().Trim())).AsQueryable();
+            if (organizationParams.Established != null && organizationParams.Established > 0)
+                query = query.Where(o => o.Established == organizationParams.Established).AsQueryable();
+            if (organizationParams.City != null)
+                query = query.Where(o => o.City.ToLower().Trim().Contains(organizationParams.City.ToLower().Trim())).AsQueryable();
+            if (organizationParams.ProvinceOrState != null)
+                query = query.Where(o => o.ProvinceOrState.ToLower().Trim().Contains(organizationParams.ProvinceOrState.ToLower().Trim())).AsQueryable();
+            if (organizationParams.Country != null)
+                query = query.Where(o => o.Country.ToLower().Trim().Contains(organizationParams.Country.ToLower().Trim())).AsQueryable();
+
             query = organizationParams.OrderBy switch
             {
                 "alphabetical" => query.OrderBy(o => o.Name),
@@ -173,6 +224,19 @@ namespace API.Data
         public async Task<PagedList<OrganizationDto>> GetAffiliatedOrganizationsAsync(OrganizationParams organizationParams, int id)
         {
             var query = _context.Users.Where(u => u.Id == id).SelectMany(u => u.Affiliation).AsQueryable();
+
+            if (organizationParams.Name != null)
+                query = query.Where(o => o.Name.ToLower().Trim().Contains(organizationParams.Name.ToLower().Trim())).AsQueryable();
+            if (organizationParams.OrgType != null)
+                query = query.Where(o => o.OrgType.ToLower().Trim().Contains(organizationParams.OrgType.ToLower().Trim())).AsQueryable();
+            if (organizationParams.Established != null && organizationParams.Established > 0)
+                query = query.Where(o => o.Established == organizationParams.Established).AsQueryable();
+            if (organizationParams.City != null)
+                query = query.Where(o => o.City.ToLower().Trim().Contains(organizationParams.City.ToLower().Trim())).AsQueryable();
+            if (organizationParams.ProvinceOrState != null)
+                query = query.Where(o => o.ProvinceOrState.ToLower().Trim().Contains(organizationParams.ProvinceOrState.ToLower().Trim())).AsQueryable();
+            if (organizationParams.Country != null)
+                query = query.Where(o => o.Country.ToLower().Trim().Contains(organizationParams.Country.ToLower().Trim())).AsQueryable();
 
             query = organizationParams.OrderBy switch
             {
